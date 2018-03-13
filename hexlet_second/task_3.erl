bb39b578a3a050c8c40d19812998baeabd54ebce
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-13 02:25:03 +0200

    Done

--- hexlet_second/task_3.erl
+++ hexlet_second/task_3.erl
@@ -9,7 +9,21 @@
 %% http://www.erlang.org/doc/man/lists.html#member-2
 member(Elem, List) ->
     %% BEGIN (write your solution here)
-    
+    case List of
+	[] ->
+	    false;
+	[H|[]] ->
+	    case H of
+		Elem -> true;
+		_ -> member(Elem, [])
+	    end;
+	[H|T] ->
+	    case H of
+		Elem -> true;
+		_ -> member(Elem, T)
+	    end;
+	_-> false
+	end.
     %% END
 
 member_test() ->
@@ -25,12 +39,40 @@ member_test() ->
 %% http://www.erlang.org/doc/man/lists.html#filter-2
 filter(Pred, List) ->
     %% BEGIN (write your solution here)
-    
-    %% END
+    filter(Pred, List ,[]).
+filter([],_,_) -> [];
+filter(_Pred,[],Result) ->
+    Result;
+filter(Pred,List,Result) ->
+    case List of
+	[H|[]] ->
+	    case Pred(H) of
+		true ->
+		    filter(Pred,[],reverse([H|Result]));
+		false ->
+		    filter(Pred,[],Result)
+	    end;
+	[H|T] ->
+	    case Pred(H) of
+		true ->
+		    filter(Pred,T,[H|Result]);
+		false ->
+		    filter(Pred,T,Result)
+	    end
+	end.
+%% END
+
+reverse(List) ->
+    reverse(List,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+
 
 filter_test() ->
     F = fun(Val) -> Val rem 5 =:= 0 end,
     ?assertEqual([], filter(F, [])),
     ?assertEqual([], filter(F, [1,2,3,4])),
-    ?assertEqual([5,10], filter(F, [1,2,3,4,5,6,7,8,9,10])),
+    ?assertEqual([5,10,15], filter(F, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])),
     ok.
